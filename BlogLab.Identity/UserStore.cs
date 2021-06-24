b84b577347028fd187067dc793ba0fa88b6e31bd
Richard Ward <richbward@gmail.com>
2021-06-24 06:45:02 -0400

    Created Identiy project with UserStore.

---
+++ BlogLab.Identity/UserStore.cs
@@ -0,0 +1,132 @@
+﻿using BlogLab.Models.Account;
+using BlogLab.Repository;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogLab.Identity
+{
+    public class UserStore :
+        IUserStore<ApplicationUserIdentity>,
+        IUserEmailStore<ApplicationUserIdentity>,
+        IUserPasswordStore<ApplicationUserIdentity>
+    {
+
+        private readonly IAccountRepository _accountRepository;
+
+        public UserStore(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public async Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await _accountRepository.CreateAsync(user, cancellationToken);
+        }
+
+        public Task<IdentityResult> DeleteAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            // Nothing to dispose
+        }
+
+        public Task<ApplicationUserIdentity> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUserIdentity> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ApplicationUserIdentity> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            return await _accountRepository.GetByUsernameAsync(normalizedUserName, cancellationToken);
+        }
+
+        public async Task<string> GetEmailAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.Email);
+        }
+
+        public async Task<bool> GetEmailConfirmedAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(true);
+        }
+
+        public async Task<string> GetNormalizedEmailAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.NormalizedEmail);
+        }
+
+        public async Task<string> GetNormalizedUserNameAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.Normalizedusername);
+        }
+
+        public async Task<string> GetPasswordHashAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.PasswordHash);
+        }
+
+        public async Task<string> GetUserIdAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.Id.ToString());
+        }
+
+        public async Task<string> GetUserNameAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.Username);
+        }
+
+        public async Task<bool> HasPasswordAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(user.PasswordHash != null);
+        }
+
+        public Task SetEmailAsync(ApplicationUserIdentity user, string email, CancellationToken cancellationToken)
+        {
+            user.Email = email;
+            return Task.FromResult(0);
+        }
+
+        public Task SetEmailConfirmedAsync(ApplicationUserIdentity user, bool confirmed, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task SetNormalizedEmailAsync(ApplicationUserIdentity user, string normalizedEmail, CancellationToken cancellationToken)
+        {
+            user.NormalizedEmail = normalizedEmail;
+            return Task.FromResult(0);
+        }
+
+        public Task SetNormalizedUserNameAsync(ApplicationUserIdentity user, string normalizedName, CancellationToken cancellationToken)
+        {
+            user.Normalizedusername = normalizedName;
+            return Task.FromResult(0);
+        }
+
+        public Task SetPasswordHashAsync(ApplicationUserIdentity user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.PasswordHash = passwordHash;
+            return Task.FromResult(0);
+        }
+
+        public Task SetUserNameAsync(ApplicationUserIdentity user, string userName, CancellationToken cancellationToken)
+        {
+            user.Username = userName;
+            return Task.FromResult(0);
+        }
+
+        public Task<IdentityResult> UpdateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
