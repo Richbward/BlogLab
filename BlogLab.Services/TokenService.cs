e8c50c9cc7cdb8cf96505c71660fbcbb188b6012
Richard Ward <richbward@gmail.com>
2021-06-24 08:13:07 -0400

    Services project completed.

---
+++ BlogLab.Services/TokenService.cs
@@ -0,0 +1,50 @@
+﻿using BlogLab.Models.Account;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Services
+{
+    public class TokenService: ITokenService
+    {
+        private readonly IConfiguration _config;
+        private readonly SymmetricSecurityKey _key;
+        private readonly string _issuer;
+
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            _issuer = _config["Jwt:Issuer"];
+            _config = config;
+        }
+
+        public string CreateToken(ApplicationUserIdentity user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.ApplicationUserId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            // In this case, both Issuer and Audience are the same - this _issuer is listed twice
+            var token = new JwtSecurityToken(
+                _issuer,
+                _issuer,
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
