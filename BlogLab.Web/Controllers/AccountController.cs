3969b96fd38060e197c2a90a5626ec3c604e2098
Richard Ward <richbward@gmail.com>
2021-06-24 10:16:34 -0400

    Initial Controller Changes.

---
+++ BlogLab.Web/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using BlogLab.Models.Account;
+using BlogLab.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogLab.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ITokenService _tokenservice;
+        private readonly UserManager<ApplicationUserIdentity> _userManager;
+        private readonly SignInManager<ApplicationIdentity> _signInManager;
+
+        public AccountController(
+            ITokenService tokenservice,
+            UserManager<ApplicationUserIdentity> userManager,
+            SignInManager<ApplicationIdentity> signInManager
+            )
+        {
+            _tokenservice = tokenservice;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ApplicationUser>> Register(ApplicatrionUserCreate applicatrionUserCreate)
+        {
+            var applicationUserIdentity = new ApplicationUserIdentity
+            {
+                Username = applicatrionUserCreate.Username,
+                Email = applicatrionUserCreate.Email,
+                Fullname = applicatrionUserCreate.Fullname
+            };
+
+            var result = await _userManager.CreateAsync(applicationUserIdentity,
+                applicatrionUserCreate.Password);
+
+            if (result.Succeeded)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Id = applicationUserIdentity.Id,
+                    Username = applicationUserIdentity.Username,
+                    Fullname = applicationUserIdentity.Fullname,
+                    Email = applicationUserIdentity.Email,
+                    Token = _tokenservice.CreateToken(applicationUserIdentity)
+                };
+
+                return user;
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -1,15 +1,25 @@
+using BlogLab.Identity;
+using BlogLab.Models.Account;
 using BlogLab.Models.Settings;
+using BlogLab.Repository;
+using BlogLab.Services;
+using BlogLab.Web.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogLab.Web
@@ -18,6 +28,7 @@ namespace BlogLab.Web
     {
         public Startup(IConfiguration configuration)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             Configuration = configuration;
         }
 
@@ -28,7 +39,50 @@ namespace BlogLab.Web
         {
             services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
 
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+
+            services.AddScoped<IBlogRepository, BlogRepository>();
+            services.AddScoped<IBlogCommentRepository, BlogCommentRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+            services.AddIdentityCore<ApplicationUserIdentity>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+            })
+                .AddUserStore<UserStore>()
+                .AddDefaultTokenProviders()
+                .AddSignInManager<SignInManager<ApplicationIdentity>>();
+
             services.AddControllers();
+            services.AddCors();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(
+                    options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = Configuration["Jwt.Issuer"],
+                            ValidAudience = Configuration["Jwt.Issuer"],
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt.Issuer"])),
+                            ClockSkew = TimeSpan.Zero
+                        };
+                    }
+                );
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BlogLab.Web", Version = "v1" });
@@ -45,8 +99,20 @@ namespace BlogLab.Web
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogLab.Web v1"));
             }
 
+            app.ConfigureExceptionHandler();
+
             app.UseRouting();
 
+            if (env.IsDevelopment())
+            {
+                app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            }
+            else
+            {
+                app.UseCors();
+            }
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
